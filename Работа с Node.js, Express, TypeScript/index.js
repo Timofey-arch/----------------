0a0cf5c6934558410baba30bfb3add4d1f9f1708
Bruh <timofejbakurov@gmail.com>
2023-05-24 11:46:07 +0700

    added documentation

--- Работа с Node.js, Express, TypeScript/index.js
+++ Работа с Node.js, Express, TypeScript/index.js
@@ -3,6 +3,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+/*
+  Задача 1
+  Создать простой сервер, который отвечал бы по запросам на точку api и отдавал бы массив пользователей в виде json.
+  Для этого было первым дело выполнено задание 2 - создать интерфейс пользователя с заданными полями.
+  Работоспособность можно проверить, перейдя по ссылке http:localhost:5000/api/users.
+  Сборка и запуск сервера осуществляется через команды в файле package.json.
+*/
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 const apiRouter = express_1.default.Router();
@@ -11,11 +18,19 @@ const users = [
     { id: 2, name: "Alice", email: "alice@gmail.com" },
     { id: 3, name: "Bob", email: "bob@mail.su" }
 ];
+/*
+  Задача 3 - создать функцию, которая складывает два числа.
+  С ее работоспособностью можно ознакомиться, перейдя по ссылке http:localhost:5000/api/sumFunction.
+*/
 function add(a, b) {
     return a + b;
 }
+/*
+  Задача 4 - создать middleware функцию, которая логирует каждый входящий запрос.
+  В логах должны быть: метод запроса, url, дата запроса.
+*/
 app.use(function (req, _, next) {
-    console.log('| Method - ' + req.method + ' | URL - ' + req.url + ' | Date - ' + new Date() + '|');
+    console.log('| Method - ' + req.method + ' | URL - ' + req.url + ' | Date - ' + new Date() + ' |');
     next();
 });
 // Main user part
--- Работа с Node.js, Express, TypeScript/index.ts
+++ Работа с Node.js, Express, TypeScript/index.ts
@@ -1,3 +1,10 @@
+/*
+  Задача 1
+  Создать простой сервер, который отвечал бы по запросам на точку api и отдавал бы массив пользователей в виде json.
+  Для этого было первым дело выполнено задание 2 - создать интерфейс пользователя с заданными полями.
+  Работоспособность можно проверить, перейдя по ссылке http:localhost:5000/api/users.
+  Сборка и запуск сервера осуществляется через команды в файле package.json.
+*/
 import express, { Express } from 'express';
 
 const app: Express = express();
@@ -15,10 +22,18 @@ const users: Array<User> = [
   {id: 3, name: "Bob", email: "bob@mail.su"}
 ];
 
+/* 
+  Задача 3 - создать функцию, которая складывает два числа.
+  С ее работоспособностью можно ознакомиться, перейдя по ссылке http:localhost:5000/api/sumFunction.
+*/
 function add(a: number, b: number): number {
   return a + b;
 }
 
+/* 
+  Задача 4 - создать middleware функцию, которая логирует каждый входящий запрос.
+  В логах должны быть: метод запроса, url, дата запроса.
+*/
 app.use(function (req, _, next) {
   console.log('| Method - ' + req.method + ' | URL - ' + req.url + ' | Date - ' + new Date() + ' |');
   next();
